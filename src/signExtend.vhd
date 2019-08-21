df158192a8c6e99ae8f28428aa16915db36139a0
Rafael <uhrafel@gmail.com>
2019-08-20 21:02:30 -0300

    signExtend

--- src/signExtend.vhd
+++ src/signExtend.vhd
@@ -9,18 +9,55 @@ entity signExtend is
 	generic(
 	  ws_in:  natural := 32; -- input word size
 		ws_out: natural := 64); -- output word size
-	port(
+	port(												
 		i: in	 bit_vector(ws_in-1  downto 0); -- input
 		o: out bit_vector(ws_out-1 downto 0)  -- output
 	);
 end signExtend;
 
 architecture combinational of signExtend is
-begin
-	lsb: for idx in 0 to (i'length-1) generate
-		o(idx) <= i(idx);
+signal AllInstruction : bit_vector(31 downto 0);
+signal instru31to21   : bit_vector(10 downto 0);
+signal BZero		  : bit_vector(18 downto 0);
+signal LwSwExtend 	  : bit_vector(8 downto 0);
+signal oLS			  : bit_vector(63 downto 0);
+signal oCBZ			  : bit_vector(63 downto 0);
+begin	
+	AllInstruction <= i;		  
+	
+	instru31to21 <= AllInstruction(31 downto 21);
+	
+	LwSwExtend <= AllInstruction(20 downto 12);
+	BZero <= AllInstruction(23 downto 5);
+	
+	
+	-------------------------------------------------------------------
+	--Extend dos 9 bits: load store
+	-------------------------------------------------------------------
+	
+	lsbLS: for idxLS in 0 to (LwSwExtend'length-1) generate
+		oLS(idxLS) <= LwSwExtend(idxLS);
 	end generate;
-	msb: for idx in (i'length) to (o'length-1) generate
-		o(idx) <= i(i'length-1);
+	msbLS: for idxLS in (LwSwExtend'length) to (oLS'length-1) generate
+		oLS(idxLS) <= LwSwExtend(LwSwExtend'length-1);
 	end generate;
+	
+	-------------------------------------------------------------------
+	--Extend dos 19 bits: CBZ (Branch on Zero)
+	-------------------------------------------------------------------
+	
+	lsbCBZ: for idxCBZ in 0 to (BZero'length-1) generate
+		oCBZ(idxCBZ) <= BZero(idxCBZ);
+	end generate; 
+	msbCBZ: for idxCBZ in (BZero'length) to (oCBZ'length-1) generate
+		oCBZ(idxCBZ) <= BZero(BZero'length-1);
+	end generate;		
+	
+	-------------------------------------------------------------------
+	--Escolha final do output
+	-------------------------------------------------------------------
+	
+	o <= oLS  when instru31to21 = "11111000010" or instru31to21 = "11111000000" else
+		 oCBZ when instru31to21(10 downto 3) = "10110100";
+		 
 end combinational;
