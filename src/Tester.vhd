ed6301d6b7e9722fc7b911c3aabac3b4b1e64d4e
Rafael <uhrafel@gmail.com>
2019-11-05 15:03:31 -0300

    Tester Pronto

--- src/Tester.vhd
+++ src/Tester.vhd
@@ -15,24 +15,30 @@ use ieee.math_real.all;										  -- OBS: para alterar a sequencia de valores a
 use types.all; -- 1 word, 32 bits							 
 
 entity tester is											 
-	generic (
-		addrSize   : natural := 10;
-		rangeBits  : natural := 4;                           -- Quantidade de bits de range no gerador aleatório de saltos
-		rand1_data : natural := 1;
-		rand2_data : natural := 2;
-		rand1_inst : natural := 21;
-		rand2_inst : natural := 22
+	generic (									
+		addrSize       : natural := 10; --Basta colocar qualquer natural dessa linha para baixo!
+		rangeBits      : natural := 4;                           -- Quantidade de bits de range no gerador aleatório de saltos
+		rand1_data     : natural := 1;
+		rand2_data     : natural := 2;
+		rand1_inst     : natural := 21;
+		rand2_inst     : natural := 22;
+		plusMinusData1 : natural := 50;
+		plusMinusData2 : natural := 51;
+		plusMinusInst1 : natural := 100;
+		plusMinusInst2 : natural := 100
 	);
     port (	
 		clk          	  : in  bit;						 -- Mesmo ciclo de clock que os caches L1
-		addressMode  	  : in  bit;                         -- Mode '0' = instruções consecutivas a partir de startAddress; '1' = instruções randomicas
+		addressMode  	  : in  bit_vector(1 downto 0);      -- Mode "00" = instruções consecutivas a partir de startAddress; "01" = instruções com offset randomico; "10" = instruções totalmente randomicas
 		cacheMode	 	  : in  bit_vector(1 downto 0);      -- Mode "01" = só cache de instruções; "10" = só cache de dados; "11" = os dois caches
 		startAddressData  : in  bit_vector(addrSize-1 downto 0);	
 		startAddressInst  : in  bit_vector(addrSize-1 downto 0);	
    		stallData	 	  : in  bit;						
 		stallInst    	  : in  bit; 
 		isBranchData	  : in  bit;
+		branchUpDownData  : in  bit;
 		isBranchInst	  : in  bit;
+		branchUpDownInst  : in  bit;
 		branchDataOffset  : in  bit_vector(addrSize-1 downto 0);
 		branchInstOffset  : in  bit_vector(addrSize-1 downto 0);
 		toTestAddressData : out bit_vector(addrSize-1 downto 0);
@@ -58,11 +64,18 @@ process(clk, stallData, stallInst)
 		variable addressInstSum   : unsigned(addrSize-1 downto 0); 
 		variable temp 			  : unsigned(addrSize-1 downto 0) := "0000000100"; 
 		variable seed1Data 	      : positive := rand1_data;
-		variable seed2Data 	      : positive:= rand2_data;
+		variable seed2Data 	      : positive := rand2_data;
 		variable seed1Inst 	      : positive := rand1_inst;
-		variable seed2Inst 	      : positive:= rand2_inst;
+		variable seed2Inst 	      : positive := rand2_inst;				
+		variable seedSignalData1  : positive := plusMinusData1;
+		variable seedSignalData2  : positive := plusMinusData2;
+		variable seedSignalInst1  : positive := plusMinusInst1;
+		variable seedSignalInst2  : positive := plusMinusInst2;
 		variable re1Data, re1Inst : integer;
 		variable re2Data, re2Inst : real;
+		variable pm1Data, pm1Inst : integer;
+		variable pm2Data, pm2Inst : real;
+		
 		
 		begin
 			if(clk'event and clk = '1') then
@@ -74,18 +87,35 @@ process(clk, stallData, stallInst)
 				else
 					if(stallData = '0') then
 						if(cacheMode(0) = '1') then															   --Geração de ends. para cache de dados ativa!
-							if(addressMode = '0' and (not isBranchData'event) and isBranchData = '0') then	   --Endereços consecutivos
+							if(addressMode = "00" and (not isBranchData'event) and isBranchData = '0') then	   --Endereços consecutivos
 								addressDataSum := unsigned(addressDataToMemory) + unsigned(temp);	           --end. += 4
 						
-							elsif(addressMode = '1' and (not isBranchData'event) and isBranchData = '0') then  --Endereços randomicos
+							elsif(addressMode = "01" and (not isBranchData'event) and isBranchData = '0') then  --Endereços randomicos
 								uniform(seed1Data, seed2Data, re2Data);
 								re1Data := integer(re2Data * real(2**rangeBits-1));
 								randomVectorData <= bit_vector(to_unsigned(re1Data, addrSize));
-								addressDataSum := unsigned(addressDataToMemory) + unsigned(randomVectorData); 
 								
-							elsif(isBranchData'event and isBranchData = '1') then							   --Endereços 
-								addressDataSum := unsigned(addressDataToMemory) + unsigned(branchDataOffset); 
-						
+								uniform(seedSignalData1, seedSignalData2, pm2Data);
+								pm1Data := integer(pm2Data * real(1));
+								
+								if(pm1data = 1) then
+									addressDataSum := unsigned(addressDataToMemory) + unsigned(randomVectorData); 
+								else
+									addressDataSum := unsigned(addressDataToMemory) - unsigned(randomVectorData); 
+								end if;
+								
+							elsif(addressMode = "10") then			   
+								uniform(seed1Data, seed2Data, re2Data);
+								re1Data := integer(re2Data * real(2**addrSize-1));
+								randomVectorData <= bit_vector(to_unsigned(re1Data, addrSize));
+								addressDataSum := unsigned(randomVectorData); 
+								
+							elsif(isBranchData'event and isBranchData = '1') then							   --Endereços 	
+								if(branchUpDownData = '1') then
+									addressDataSum := unsigned(addressDataToMemory) + unsigned(branchDataOffset); 
+								else
+									addressDataSum := unsigned(addressDataToMemory) - unsigned(branchDataOffset); 
+								end if;
 							end if;
 						end if;
 					end if;
@@ -94,18 +124,35 @@ process(clk, stallData, stallInst)
 					
 					if(stallInst = '0') then
 						if(cacheMode(1) = '1') then		--Geração de ends. para cache de inst ativa!
-							if(addressMode = '0' and (not isBranchInst'event) and isBranchInst = '0') then
+							if(addressMode = "00" and (not isBranchInst'event) and isBranchInst = '0') then
 								addressInstSum := unsigned(addressInstToMemory) + unsigned(temp);	         --end. += 4
 						
-							elsif(addressMode = '1' and (not isBranchInst'event) and isBranchInst = '0') then
+							elsif(addressMode = "01" and (not isBranchInst'event) and isBranchInst = '0') then
 								uniform(seed1Inst, seed2Inst, re2Inst);
 								re1Inst := integer(re2Inst * real(2**rangeBits-1));
 								randomVectorInst <= bit_vector(to_unsigned(re1Inst, addrSize));
-								addressInstSum := unsigned(addressInstToMemory) + unsigned(randomVectorInst); 
+								
+								uniform(seedSignalInst1, seedSignalInst2, pm2Inst);
+								pm1Inst := integer(pm2Inst * real(1));
+								
+								if(pm1Inst = 1) then
+									addressInstSum := unsigned(addressInstToMemory) + unsigned(randomVectorInst); 
+								else
+									addressInstSum := unsigned(addressInstToMemory) - unsigned(randomVectorInst); 
+								end if;
+								
+							elsif(addressMode = "10") then			   
+								uniform(seed1Inst, seed2Inst, re2Inst);
+								re1Inst := integer(re2Inst * real(2**addrSize-1));
+								randomVectorInst <= bit_vector(to_unsigned(re1Inst, addrSize));
+								addressInstSum := unsigned(randomVectorInst); 
 								
 							elsif(isBranchInst'event and isBranchInst = '1') then
-								addressInstSum := unsigned(addressInstToMemory) + unsigned(branchInstOffset); 
-						
+								if(branchUpDownInst = '1') then
+									addressInstSum := unsigned(addressInstToMemory) + unsigned(branchInstOffset); 
+								else
+									addressInstSum := unsigned(addressInstToMemory) - unsigned(branchInstOffset); 
+								end if;
 							end if;
 						end if;
 					end if;
