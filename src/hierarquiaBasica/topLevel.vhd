9e415dcd8a043913c6a05c897f6544e3052f15f0
Rafael <uhrafel@gmail.com>
2019-11-28 10:41:06 -0300

    top level da hierarquia basica

--- src/hierarquiaBasica/topLevel.vhd
+++ src/hierarquiaBasica/topLevel.vhd
@@ -33,7 +33,6 @@ entity topLevel is
 		clkI			  : in  bit;	--Só vou simular com clkI msm por enquanto...
 		clkD			  : in  bit;			 
 		restartAddr		  : in  bit;
-		fullCache		  : in  bit;
 		--Relacionados ao pipeline (Dados)	  
 		dataInD 		  : in  word_type;
 		cpuWrite		  : in  bit;
@@ -108,7 +107,6 @@ component tester is
     port (	
 		clk          	  : in  bit;						 -- Mesmo ciclo de clock que os caches L1
 		restartAddr	      : in  bit;
-		fullCache		  : in  bit;
 		addressMode  	  : in  bit_vector(1 downto 0);      -- Mode "00" = instruções consecutivas a partir de startAddress; "01" = instruções com offset randomico; "10" = instruções totalmente randomicas
 		cacheMode	 	  : in  bit_vector(1 downto 0);      -- Mode "01" = só cache de instruções; "10" = só cache de dados; "11" = os dois caches
 		startAddressData  : in  bit_vector(addrSize-1 downto 0);	
@@ -141,7 +139,7 @@ begin
 	
 	addressGenerator : tester generic map(addrSize, rangeBits, rand1_data, rand2_data, rand1_inst, rand2_inst, plusMinusData1, plusMinusData2,
 											plusMinusInst1, plusMinusInst2)
-    						  port map(clkPipeline, restartAddr, fullCache, addressMode, cacheMode, startAddressData, startAddressInst, endAddressData, endAddressInst, 
+    						  port map(clkPipeline, restartAddr, addressMode, cacheMode, startAddressData, startAddressInst, endAddressData, endAddressInst, 
 										setAddressEnable, setAddressData, setAddressInst, stallData, stallInst, isBranchData, branchUpDownData, isBranchInst, branchUpDownInst, branchDataOffset, branchInstOffset, cpuAddrD,
 										cpuAddrI);
 	toTestAddressData <= cpuAddrD;
